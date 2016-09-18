34b50255275d097250f4c727b631cb9dce598e17
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-09-18 16:44:03 +0200

    Language short description is given as a registered callable docstring.

--- txtools/cli/list_langs.py
+++ txtools/cli/list_langs.py
@@ -3,9 +3,17 @@ from textx.lang import iter_languages
 
 
 @click.command()
-def list_langs():
+@click.option('-v', '--verbose', is_flag=True, default=False,
+              help='Print description of each language.')
+def list_langs(verbose):
     """
     Lists registered textX languages.
     """
     for ep in iter_languages():
-        click.echo("%s - %s (%s)" % (ep.name, ep.dist, ep.dist.location))
+        desc = ""
+        if verbose:
+            lang_doc = ep.load().__doc__
+            if lang_doc:
+                desc = "\n\t{}".format(ep.load().__doc__)
+        click.echo("%s - %s (%s)%s" % (ep.name, ep.dist, ep.dist.location,
+                                       desc))
