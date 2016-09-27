c95ef55f4bf615ddd48541e5e7d6b96d30654c1f
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-09-27 23:31:33 +0200

    Verbose option for listing generators.

--- txtools/cli/list_gens.py
+++ txtools/cli/list_gens.py
@@ -3,9 +3,17 @@ from txtools.gen import iter_generators
 
 
 @click.command()
-def list_gens():
+@click.option('-v', '--verbose', is_flag=True, default=False,
+              help='Print description of each generator.')
+def list_gens(verbose):
     """
     Lists registered textX code generators.
     """
     for ep in iter_generators():
-        click.echo("%s - %s (%s)" % (ep.name, ep.dist, ep.dist.location))
+        desc = ""
+        if verbose:
+            gen_doc = ep.load().__doc__
+            if gen_doc:
+                desc = "\n\t{}".format(gen_doc.strip())
+        click.echo("%s - %s (%s)%s" % (ep.name, ep.dist, ep.dist.location,
+                                       desc))
