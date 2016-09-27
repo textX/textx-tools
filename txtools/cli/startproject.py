0503e2e705619e4e8f8ea84060d4c12e2ccb3f57
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-09-27 23:33:52 +0200

    Generator project should have language specified.
    Generator always depends to a particular language.

--- txtools/cli/startproject.py
+++ txtools/cli/startproject.py
@@ -1,42 +1,71 @@
 import os
+import sys
 import re
 import click
 from jinja2 import Environment, PackageLoader
+from textx.lang import get_language
+from textx.exceptions import TextXError
 
 
 @click.command()
 @click.option('-t', '--project-type', default='lang',
               type=click.Choice(['lang', 'gen']))
+@click.option('-l', '--language',
+              help='Language for generator projects.')
 @click.argument('project_name')
-def startproject(project_type, project_name):
+def startproject(project_type, language, project_name):
     """
     Generates scaffolding for textX projects.
     """
-    if not _check_project_name(project_name):
-        raise Exception('Project name may contain letters, digits and dashes.')
+    if project_type == 'gen':
+        if not language:
+            click.echo('Language must be specified for generator'
+                       ' projects. See -l switch.')
+            sys.exit(1)
+        else:
+            try:
+                get_language(language)
+            except TextXError as e:
+                click.echo(e)
+                click.echo('Try to install it with "pip install textx-lang-{}"'
+                           .format(language))
+                click.echo('Use "textx list-langs" to see all registered '
+                           'languages.')
+                sys.exit(1)
 
-    full_project_name = 'textx-{}-{}'.format(project_type, project_name)
+    if not _check_project_name(project_name):
+        click.echo('Project name may contain letters, digits and dashes.')
+        sys.exit(1)
+
+    if project_type == 'lang':
+        package_name = _project_name_to_package_name(project_name)
+        full_project_name = 'textx-{}-{}'.format(project_type, project_name)
+    else:
+        package_name = "{}_{}".format(
+            language, _project_name_to_package_name(project_name))
+        full_project_name = 'textx-{}-{}-{}'.format(project_type,
+                                                    language,
+                                                    project_name)
+
     project_type_full = 'language' if project_type == 'lang' else 'generator'
 
     click.echo('Generating project for {} {} in folder {}.'
                .format(project_type_full, project_name, full_project_name))
 
-    _generate_base(project_name, full_project_name)
+    _generate_base(project_name, full_project_name, package_name)
 
     if project_type == 'lang':
-        _start_lang(project_name, full_project_name)
+        _start_lang(project_name, full_project_name, package_name)
     else:
-        _start_gen(project_name, full_project_name)
+        _start_gen(project_name, full_project_name, package_name, language)
 
     click.echo('Done.')
 
 
-def _start_lang(project_name, full_project_name):
+def _start_lang(project_name, full_project_name, package_name):
     """
     Generates scaffolding for 'language' textX project.
     """
-    package_name = _project_name_to_package_name(project_name)
-
     current_dir = os.curdir
     project_folder = os.path.join(current_dir, full_project_name)
 
@@ -49,7 +78,8 @@ def _start_lang(project_name, full_project_name):
 
     t = env.get_template('lang.py')
     with open(os.path.join(project_folder, package_name, 'lang.py'), 'w') as f:
-        f.write(t.render(metamodel_file='{}.tx'.format(package_name)))
+        f.write(t.render(metamodel_file='{}.tx'.format(package_name),
+                         lang_name=package_name))
 
     t = env.get_template('lang.tx')
     with open(os.path.join(project_folder, package_name,
@@ -57,12 +87,10 @@ def _start_lang(project_name, full_project_name):
         f.write(t.render())
 
 
-def _start_gen(project_name, full_project_name):
+def _start_gen(project_name, full_project_name, package_name, language):
     """
     Generates scaffolding for 'generator' textX project.
     """
-    package_name = _project_name_to_package_name(project_name)
-
     current_dir = os.curdir
     project_folder = os.path.join(current_dir, full_project_name)
 
@@ -73,14 +101,15 @@ def _start_gen(project_name, full_project_name):
         f.write(t.render(full_project_name=full_project_name,
                          package_name=package_name))
 
+    t = env.get_template('gen.py')
+    with open(os.path.join(project_folder, package_name, 'gen.py'), 'w') as f:
+        f.write(t.render(package_name=package_name, lang_name=language))
 
-def _generate_base(project_name, project_name_full):
+
+def _generate_base(project_name, project_name_full, package_name):
     """
     Generates base scaffolding.
     """
-
-    package_name = _project_name_to_package_name(project_name)
-
     current_dir = os.curdir
     project_folder = os.path.join(current_dir, project_name_full)
 
