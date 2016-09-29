6d66faa3a0363c7bec7e603771f2f365a38a5cb8
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-09-29 18:24:18 +0200

    Handling exceptions in generate subcommand.

--- txtools/cli/generate.py
+++ txtools/cli/generate.py
@@ -1,7 +1,8 @@
-import os, sys
+import os
 import click
 from txtools.lang.genconf import load_genconf
 from txtools.gen import generate
+from txtools.exception import TextXToolsException
 
 
 @click.command()
@@ -11,27 +12,33 @@ def generate(project_folder):
     """
     Runs source code generator.
     """
-    project_folder = project_folder if project_folder else os.curdir
+    try:
 
-    # Check if genconf folder exists
-    if not os.path.exists(os.path.join(project_folder, 'genconf')):
-        raise TextXToolsException('There is no "genconf" folder here. '
-                                  'Is this a textX project?')
+        project_folder = project_folder if project_folder else os.curdir
 
-    click.echo("Generating application code.")
+        # Check if genconf folder exists
+        if not os.path.exists(os.path.join(project_folder, 'genconf')):
+            raise TextXToolsException('There is no "genconf" folder here. '
+                                    'Is this a textX project?')
 
-    # Load each genconf file. Merge with base genconf for the generator.
-    for root, dirs, files in os.walk(project_folder):
-        for f in files:
-            _, ext = os.path.splitext(f)
-            if not ext == 'genconf':
-                continue
-            gc_model = load_genconf(f)
+        click.echo("Generating application code.")
 
-            # For each model configured in current genconf
-            for model in gc_model.models:
-                generate(model, )
 
+        # Load each genconf file. Merge with base genconf for the generator.
+        for root, dirs, files in os.walk(project_folder):
+            for f in files:
+                _, ext = os.path.splitext(f)
+                if not ext == 'genconf':
+                    continue
+                gc_model = load_genconf(f)
+
+                # For each model configured in current genconf
+                for model in gc_model.models:
+                    generate(model, )
+
+    except TextXToolsException as e:
+        click.echo(e)
+        sys.exit(1)
 
     # For each referenced model, load model using appropriate language.
     # Calls generate for the given model and given genconf.
